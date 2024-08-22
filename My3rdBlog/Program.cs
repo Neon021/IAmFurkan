41af6105e071650251d6caf9204730503dc1c29e
Furkan Bilal Yiğit <bilalyigit594@gmail.com>
2024-08-22 18:38:31 +0300

    Feature(IdentityRole): Implemented services utilized in Program.cs instead of old hardcoded process

--- My3rdBlog/Program.cs
+++ My3rdBlog/Program.cs
@@ -1,11 +1,8 @@
-using Blog.Data;
+using Blog.Helpers.Startup;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection;
 using My3rdBlog;
-using System;
-using System.Linq;
+using System.Threading.Tasks;
 
 namespace Blog
 {
@@ -14,40 +11,8 @@ namespace Blog
         public static void Main(string[] args)
         {
             var host = CreateWebHostBuilder(args).Build();
-            try
-            {
-                var scope = host.Services.CreateScope();
 
-                var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-                var roleMgr = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-                ctx.Database.EnsureCreated();
-
-                var adminRole = new IdentityRole("Admin"); //Create "Admin" role to get claimed
-                if (!ctx.Roles.Any())
-                {
-                    //create a role
-                    roleMgr.CreateAsync(adminRole).GetAwaiter().GetResult();
-                }
-
-                if (!ctx.Users.Any(u => u.UserName == "admin")) //If there's no user role named "admin" create
-                {
-                    //create an admin
-                    var adminUser = new IdentityUser
-                    {
-                        UserName = "admin",
-                        Email = "admin@test.com"
-                    };
-                    var result = userMgr.CreateAsync(adminUser, "password").GetAwaiter().GetResult();
-                    //assign Admin role to admin user
-                    userMgr.AddToRoleAsync(adminUser, adminRole.Name).GetAwaiter().GetResult();//Assign "admin" user to "Admin" role
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Task<bool> roleResult = IdentityHelper.InitAsync(host);
             host.Run();
         }
 
