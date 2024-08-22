f938b0441e7d36423495818c53ea2ecc999ee74a
Furkan Bilal Yiğit <bilalyigit594@gmail.com>
2024-08-22 18:37:03 +0300

    Feature(IdentityRole): Admin role verify process moved to a separate helper

---
+++ My3rdBlog/Helpers/Startup/AdminRoleInit.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace Blog.Helpers.Startup
+{
+    public static class AdminRoleInit
+    {
+        public static async Task<bool> InitializeAsync(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            Task task = Task.Run(async () =>
+            {
+                string adminRole = "Admin";
+                string adminUserName = configuration["AdminCredentials:UserName"];
+                string adminEmail = configuration["AdminCredentials:Email"];
+                string adminPassword = configuration["AdminCredentials:Password"];
+
+                if (!await roleManager.RoleExistsAsync(adminRole))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(adminRole));
+                }
+
+                if (await userManager.FindByEmailAsync(adminEmail) == null)
+                {
+                    IdentityUser adminUser = new()
+                    {
+                        UserName = adminUserName,
+                        Email = adminEmail
+                    };
+
+                    IdentityResult result = await userManager.CreateAsync(adminUser, adminPassword);
+
+                    if (result.Succeeded)
+                    {
+                        await userManager.AddToRoleAsync(adminUser, adminRole);
+                    }
+                }
+            });
+
+            await task;
+            return task.IsCompletedSuccessfully;
+        }
+    }
+}
